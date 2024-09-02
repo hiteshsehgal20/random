ec9e4487dfa7f2af8d082b0ec59038393211f96b
hiteshsehgal <sehgal.hitesh1991@gmail.com>
2024-09-02 22:16:54 +0100

    ClosestPalindrome

--- problems/src/main/java/Tester.java
+++ problems/src/main/java/Tester.java
@@ -1,9 +1,11 @@
 public class Tester {
     public static void main(String[] args) {
         RepeatStringForSubstring rsfm = new RepeatStringForSubstring();
-        System.out.println(rsfm.getNumberOfTimesAStringRepeatsToCreateASubstringNaive("ABCDABD", "ABCABCDABABCDABCDABD"));
+        //System.out.println(rsfm.getNumberOfTimesAStringRepeatsToCreateASubstringNaive("bcabc", "abcabcabcabcabcabcabc"));
         //abcdabd length = 7
         //ABCABCDABABCDABCDABD length = 20/7
-
+        FibonacciSeries fib = new FibonacciSeries();
+        //System.out.println(fib.getFibonacciByRecursion(50));
+        System.out.println(fib.getFibEfficient(50));
     }
 }
