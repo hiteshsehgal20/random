b55748823fd05c554adb0c394ab454ad3f481632
hiteshsehgal <sehgal.hitesh1991@gmail.com>
2024-09-27 19:38:05 +0100

    Test

---
+++ problems/src/main/java/privateConstructor/ExtendingClassToPrivate.java
@@ -0,0 +1,4 @@
+package privateConstructor;
+
+public class ExtendingClassToPrivate {
+}
---
+++ problems/src/main/java/privateConstructor/PrivateConstructorDemo.java
@@ -0,0 +1,9 @@
+package privateConstructor;
+
+public class PrivateConstructorDemo {
+/*    private PrivateConstructorDemo() {
+        System.out.println("I am in private constructor of base class");
+    }
+
+    }
+}*/}
